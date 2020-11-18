0a1d156d34a03132e2e18fc0a5b0222699b706b3
Joe Kim <joe.kim509@gmail.com>
2020-11-17 21:54:26 -0800

    testing out os path utilities

--- workProj.py
+++ workProj.py
@@ -1,8 +1,17 @@
-import re, csv
+import re, csv, os
+
+CSV_FILENAME = "salesData.csv"
+print("os.getcwd() = {}", os.getcwd())
+print("__file__ = {}", __file__)
+print("os.path.dirname(__file__) = {}", os.path.dirname(__file__))
+
+fileLocalDir = os.path.dirname(__file__)
+absoluteCsvFilePath = os.path.join(fileLocalDir, CSV_FILENAME)
+print(absoluteCsvFilePath)
 
 #1st step. Read the salesData.csv and turn the data into a Dict
 myDict = {}
->>> with open("salesData.csv", newline="") as csvfile:
-	reader = csv.reader(csvfile)
-	for row in reader:
-		myDict[row[0]] = row[1]
+with open(absoluteCsvFilePath, newline="") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        myDict[row[0]] = row[1]
