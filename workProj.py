542a006ec90bb635b41de4b0bf565f4fdefe7774
Phu Dinh <53196661+PhuDDinh@users.noreply.github.com>
2020-11-17 22:57:39 -0700

    workProj.py

--- workProj.py
+++ workProj.py
@@ -1,8 +1,10 @@
-import re, csv
+import re, csv, os
 
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 #1st step. Read the salesData.csv and turn the data into a Dict
 myDict = {}
->>> with open("salesData.csv", newline="") as csvfile:
+with open("salesData.csv", newline="") as csvfile:
 	reader = csv.reader(csvfile)
 	for row in reader:
 		myDict[row[0]] = row[1]
