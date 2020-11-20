d4eb5cdf2784538ea6620a06d2fc05e4b4aeacb7
Phu Dinh <53196661+PhuDDinh@users.noreply.github.com>
2020-11-19 19:10:07 -0700

    added 2nd step, finished getting final values for the Dict

--- workProj.py
+++ workProj.py
@@ -1,17 +1,33 @@
 import re, csv, os
+os.chdir("c:\\users\\pdinh\\python\\workproject") #for working in shell
+#CSV_FILENAME = "salesData.csv"
+#"os.getcwd() = {}", os.getcwd()
+#"__file__ = {}", __file__
+#"os.path.dirname(__file__) = {}", os.path.dirname(__file__)
 
-CSV_FILENAME = "salesData.csv"
-print("os.getcwd() = {}", os.getcwd())
-print("__file__ = {}", __file__)
-print("os.path.dirname(__file__) = {}", os.path.dirname(__file__))
+#fileLocalDir = os.path.dirname(__file__)
+#absoluteCsvFilePath = os.path.join(fileLocalDir, CSV_FILENAME)
 
-fileLocalDir = os.path.dirname(__file__)
-absoluteCsvFilePath = os.path.join(fileLocalDir, CSV_FILENAME)
-print(absoluteCsvFilePath)
 
-#1st step. Read the salesData.csv and turn the data into a Dict
+#1st step. Read the salesData.csv and turn the data into a Dict.
 myDict = {}
-with open(absoluteCsvFilePath, newline="") as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        myDict[row[0]] = row[1]
+with open("salesData.csv", newline="") as csvfile:
+	reader = csv.reader(csvfile)
+	for row in reader:
+		myDict[row[0]] = row[1]
+
+#2nd step. Use regex to return a new Dict with the same keys but the values trimmed to be only Dates.
+def myDictRegex(dict):
+	datesDict = {}
+	dateRegex = re.compile(r"\d{1,2}/\d{1,2}(/\d{1,4}){0,1}")
+	for k, v in dict.items():
+		dates = dateRegex.search(v)
+		datesDict[k] = dates.group()
+	return datesDict
+
+	
+finalDict = myDictRegex(myDict)
+print(finalDict)
+
+
+
