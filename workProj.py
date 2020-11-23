859277c078f503fb43f3d1b85b481d06f8d76357
Phu Dinh <53196661+PhuDDinh@users.noreply.github.com>
2020-11-22 17:19:45 -0700

    added 3rd step, use dates lib to compare current dates to datesDict's values and got final list of all orders that's past current date

--- workProj.py
+++ workProj.py
@@ -1,4 +1,6 @@
 import re, csv, os
+from datetime import datetime, date, time
+
 os.chdir("c:\\users\\pdinh\\python\\workproject") #for working in shell
 #CSV_FILENAME = "salesData.csv"
 #"os.getcwd() = {}", os.getcwd()
@@ -18,16 +20,30 @@ with open("salesData.csv", newline="") as csvfile:
 
 #2nd step. Use regex to return a new Dict with the same keys but the values trimmed to be only Dates.
 def myDictRegex(dict):
-	datesDict = {}
+	x = {}
 	dateRegex = re.compile(r"\d{1,2}/\d{1,2}(/\d{1,4}){0,1}")
 	for k, v in dict.items():
 		dates = dateRegex.search(v)
-		datesDict[k] = dates.group()
-	return datesDict
+		x[k] = dates.group()
+	return x
 
-	
-finalDict = myDictRegex(myDict)
-print(finalDict)
+datesDict = myDictRegex(myDict) #currently only matching the first date if there's multiple dates in the notes.
+
+#3rd step. Use dates lib to compare current date to datesDict's values. If the dates of datesDict is older than current date than append keys to a list.
+present = datetime.today()	
+def compareDates(dict):
+	x = []
+	for k, v in dict.items():
+		v += "/20" 
+		vDates = datetime.strptime(v, "%m/%d/%y")
+		if present > vDates:
+			x.append(k)
+	return x
+#This should cover 99% of case. Required data of month/day only. 
+listOfPastDates = compareDates(datesDict) #final list of all past due orders
+#4th and last step. Spit the final list into a new csv file. 
+
+
 
 
 
